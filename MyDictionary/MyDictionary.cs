4fc5ee7f5079d964f41295cc848b2604c0806292
Sultannur <200201043@kocaeli.edu.tr>
2022-08-05 00:13:04 +0300

    MyDictionary

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    internal class MyDictionary<T,K>
+    {
+        T[] array1;
+        T[] tmparray1;
+        K[] array2;
+        K[] tmparray2;
+        public MyDictionary()
+        {
+            array1 = new T[0];
+            array2 = new K[0];
+
+        }
+        public void Add(T item1, K item2)
+        {
+            tmparray1 = array1;
+            array1 = new T[array1.Length + 1];
+            tmparray2 = array2;
+            array2 = new K[array2.Length + 1];
+
+            for (int i = 0; i < tmparray1.Length; i++)
+            {
+                array1[i]=tmparray1 [i];
+                array2[i]=tmparray2 [i];
+            }
+            array1[array1.Length-1]=item1 ;
+            array2[array2.Length-1]=item2 ;
+
+        }
+
+
+        public int Count { 
+            get { return array1.Length; } 
+        }  
+
+        
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace MyDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary <int,string> dictionary1 = new MyDictionary<int, string>();
+            dictionary1.Add(1, "Ahmet");
+            dictionary1.Add(2, "Ali");
+            Console.WriteLine(dictionary1.Count);
+        }
+    }
+}
